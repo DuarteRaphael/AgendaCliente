4365892a1666a526a00057d7f527a1fd4ddb4532
Raphael Duarte <raphael-duarte@outlook.com>
2024-06-13 05:54:37 -0300

    Adicionar arquivos de projeto.

---
+++ AgendaCliente.Service.Api/Controllers/AgendasClientesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaCliente.Service.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgendasClientesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Cadastrar()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult Editar()
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult Apagar()
+        {
+            return Ok();
+        }
+        [HttpGet("page/limit")]
+        public IActionResult BuscarTodos(int page, int limit)
+        {
+            return Ok();
+        }
+        [HttpGet("id")]
+        public IActionResult BuscarPorId(Guid id)
+        {
+            return Ok();
+        }
+    }
+}
